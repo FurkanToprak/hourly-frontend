d91a94f88ea8b171d019f635a4d054cf7b776f0c
Furkan Toprak <furkancemaltoprak@gmail.com>
2022-02-28 13:35:37 -0600

    full page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Title } from '../components/utils/Texts';
 
 export default function Home() {
-    return <div>
+    return <div style={{ flex: 1 }}>
         <Title size='xl' color='purple'>Welcome!</Title>
     </div>
 }
