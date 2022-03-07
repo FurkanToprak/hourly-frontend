09b930f0b0edc9fee07eef5b3cf3f884192ec031
Furkan Toprak <furkancemaltoprak@gmail.com>
2022-03-06 23:44:12 -0600

    pre auth.

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Page from '../components/utils/Page';
+import Panel from '../components/utils/Panel';
 import { Title } from '../components/utils/Texts';
 
 export default function LogIn() {
   return (
     <Page centerY fullHeight>
       <Title>Log In</Title>
+      <Panel centerX centerY>
+        {/**  */}
+      </Panel>
     </Page>
   );
 }
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { StandardInput } from '../components/utils/Inputs';
 import Page from '../components/utils/Page';
 import Panel from '../components/utils/Panel';
-import { Body, Title } from '../components/utils/Texts';
+import { Title } from '../components/utils/Texts';
 
 export default function SignUp() {
   return (
     <Page centerY>
       <Title>Sign Up</Title>
       <Panel centerX centerY>
-        <Body>Trial</Body>
+        {/* <Body>sign in with Google</Body> */}
       </Panel>
     </Page>
   );
