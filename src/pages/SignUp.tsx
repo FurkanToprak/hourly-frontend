7948efc2eac16f0ff8e23aa62bbed0ed9b205776
Furkan Toprak <furkancemaltoprak@gmail.com>
2022-03-07 00:32:31 -0600

    fixed lint.

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import GoogleOAuth from '../components/auth/GoogleOAuth';
-import { StandardInput } from '../components/utils/Inputs';
 import Page from '../components/utils/Page';
 import Panel from '../components/utils/Panel';
 import { Title } from '../components/utils/Texts';
