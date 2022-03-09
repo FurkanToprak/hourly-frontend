62bd03bbbb45cae061b62edf2aa1a8ec214f9b13
Furkan Toprak <furkancemaltoprak@gmail.com>
2022-03-08 21:11:38 -0600

    ready to merge.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { PurpleButton, RaspberryButton } from '../components/utils/Buttons';
 import Page from '../components/utils/Page';
 import Panel from '../components/utils/Panel';
 import { Body, Title } from '../components/utils/Texts';
 
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <Page>
       <Panel centerY flex="column">
-        <Title size="xl" color="raspberry">hourly</Title>
+        <Title size="xl" color="raspberry">Hourly</Title>
         <Body size="m">A feature-rich planner that automates your tasks.</Body>
       </Panel>
       <Panel centerY flex="column">
@@ -18,8 +20,8 @@ export default function Home() {
       </Panel>
       <Panel centerY flex="column">
         <Title size="l" color="purple">Join communities</Title>
-        <Body size="m">Compare your schedules and productivity with your peers.</Body>
-        <Body size="m">Schedule meetings with friends in one click.</Body>
+        <Body size="m">Compare schedules and productivity with your peers.</Body>
+        <Body size="m">Schedule meetings with friends in just one click.</Body>
       </Panel>
       <Panel centerY flex="column">
         <Title size="l" color="purple">Goodbye, other apps!</Title>
@@ -32,9 +34,27 @@ export default function Home() {
           margin: 10, display: 'flex', width: '50%', flexDirection: 'column',
         }}
         >
-          <RaspberryButton fullWidth variant="outlined">Sign Up</RaspberryButton>
+          <RaspberryButton
+            onMouseDown={() => {
+              navigate('/signup');
+            }}
+            fullWidth
+            variant="outlined"
+          >
+            Sign Up
+
+          </RaspberryButton>
           <div style={{ height: 10 }} />
-          <PurpleButton fullWidth variant="outlined">Log In</PurpleButton>
+          <PurpleButton
+            onMouseDown={() => {
+              navigate('/login');
+            }}
+            fullWidth
+            variant="outlined"
+          >
+            Log In
+
+          </PurpleButton>
         </div>
       </Panel>
     </Page>
