12d4a52f924c07c5892889d6f5e6c3da9247f96e
Furkan Toprak <furkancemaltoprak@gmail.com>
2022-03-14 16:08:16 -0500

    resize + drop done.

--- src/components/calendar/DashboardCalendar.tsx
+++ src/components/calendar/DashboardCalendar.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
+import React, { useState } from 'react';
+import { Calendar, Event, momentLocalizer } from 'react-big-calendar';
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 import moment from 'moment';
 import Panel from '../utils/Panel';
@@ -12,6 +12,13 @@ const localizer = momentLocalizer(moment);
 const DnDCalendar = withDragAndDrop(Calendar);
 
 export default function DashboardCalendar() {
+  const [events, setEvents] = useState([
+    {
+      title: 'example Event',
+      start: moment().toDate(),
+      end: moment().add(1, 'day').toDate(),
+    },
+  ] as Event[]);
   return (
     <Panel centerY flex="column" fill>
       {/** eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -19,20 +26,28 @@ export default function DashboardCalendar() {
       <DnDCalendar
         defaultDate={moment().toDate()}
         defaultView="month"
-        onEventDrop={() => {
-          //
+        onEventDrop={(droppedEvent) => {
+          const oldEvent = droppedEvent.event as Event;
+          const freshEvents = events.filter((value) => value !== oldEvent);
+          freshEvents.push({
+            start: new Date(droppedEvent.start),
+            end: new Date(droppedEvent.end),
+            title: oldEvent.title,
+          });
+          setEvents(freshEvents);
         }}
-        onEventResize={() => {
-          //
+        onEventResize={(resizedEvent) => {
+          const oldEvent = resizedEvent.event as Event;
+          const freshEvents = events.filter((value) => value !== oldEvent);
+          freshEvents.push({
+            start: new Date(resizedEvent.start),
+            end: new Date(resizedEvent.end),
+            title: oldEvent.title,
+          });
+          setEvents(freshEvents);
         }}
         localizer={localizer}
-        events={[
-          {
-            start: moment().toDate(),
-            end: moment().add(1, 'days').toDate(),
-            title: 'Furkan eats booty',
-          },
-        ]}
+        events={events}
         startAccessor="start"
         endAccessor="end"
         resizable
