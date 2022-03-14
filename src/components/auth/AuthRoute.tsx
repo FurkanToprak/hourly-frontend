a992acdb1dd2706c3c744afb76e57966498af45b
Furkan Toprak <furkancemaltoprak@gmail.com>
2022-03-13 21:28:50 -0500

    done but maybe server broken?

--- src/components/auth/AuthRoute.tsx
+++ src/components/auth/AuthRoute.tsx
@@ -1,27 +1,25 @@
-import React from 'react'
-import { Navigate, Route, useLocation } from 'react-router-dom';
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../../contexts/Auth';
 
 export function AuthOnlyRoute(props: {
-    children: any;
-    path: string;
+    children: JSX.Element;
 }) {
-    const { isLoggedIn } = useAuth()
-    if (isLoggedIn) {
-        const location = useLocation();
-        return <Navigate to='/login' state={{ from: location }} replace/>
-    }
-    return <Route {...props}/>
+  const { isLoggedIn } = useAuth();
+  if (!isLoggedIn) {
+    const location = useLocation();
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+  return props.children;
 }
 
-export function AuthBannedRoute( props: {
-    children: any;
-    path: string;
+export function AuthBannedRoute(props: {
+  children: JSX.Element;
 }) {
-    const { isLoggedIn } = useAuth()
-    if (!isLoggedIn) {
-        const location = useLocation();
-        return <Navigate to='/' state={{ from: location }} replace/>
-    }
-    return <Route {...props}/>
+  const { isLoggedIn } = useAuth();
+  if (isLoggedIn) {
+    const location = useLocation();
+    return <Navigate to="/" state={{ from: location }} replace />;
+  }
+  return props.children;
 }
