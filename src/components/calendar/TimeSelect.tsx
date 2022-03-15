65140a7a7608a80d0e85162d72b16b09e8b749cb
Furkan Toprak <furkancemaltoprak@gmail.com>
2022-03-14 19:30:32 -0500

    timepicker fixed.

--- src/components/calendar/TimeSelect.tsx
+++ src/components/calendar/TimeSelect.tsx
@@ -1,32 +1,23 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
-import { useTheme } from '../../contexts/Theme';
-import { thinDarkBorder, thinLightBorder } from '../../styles/Theme';
-import { Body } from '../utils/Texts';
 import '../../styles/TimeSelect.css';
+import { StandardInput } from '../utils/Inputs';
 
 export default function TimeSelect(props: {
     label: string;
     onDateChange: (newDate: Date) => void
   }) {
   const [selectedDate, setSelectedDate] = useState(new Date());
-  const { theme } = useTheme();
-  const themeBorder = theme === 'light' ? thinLightBorder : thinDarkBorder;
   return (
-    <div style={{
-      flex: 1, borderRadius: 5, zIndex: 9999, border: themeBorder, display: 'flex', flexDirection: 'column', alignItems: 'center',
-    }}
-    >
-      <Body>{props.label}</Body>
-      <DatePicker
-        showTimeSelect
-        selected={selectedDate}
-        value={selectedDate.toString()}
-        onChange={(date: Date) => {
-          setSelectedDate(date);
-          props.onDateChange(date);
-        }}
-      />
-    </div>
+    <DatePicker
+      showTimeSelect
+      selected={selectedDate}
+      value={selectedDate.toString()}
+      onChange={(date: Date) => {
+        setSelectedDate(date);
+        props.onDateChange(date);
+      }}
+      customInput={<StandardInput label={props.label} fullWidth />}
+    />
   );
 }
