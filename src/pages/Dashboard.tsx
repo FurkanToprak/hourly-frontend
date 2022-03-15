0fe1eeed4d62b3feedf6db7b544b93e99019030e
Furkan Toprak <furkancemaltoprak@gmail.com>
2022-03-15 12:49:45 -0500

    import button.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import DownloadIcon from '@mui/icons-material/Download';
 import DashboardCalendar from '../components/calendar/DashboardCalendar';
 import Page from '../components/utils/Page';
 import { Title } from '../components/utils/Texts';
+import { useTheme } from '../contexts/Theme';
+import { black, white } from '../styles/Theme';
 
 export default function Dashboard() {
+  const { theme } = useTheme();
+  const themeFont = theme === 'light' ? black : white;
   return (
     <Page fullHeight centerY>
       <Title>Dashboard</Title>
+      <div style={{
+        width: '100%', display: 'flex', justifyContent: 'right', marginRight: 30,
+      }}
+      >
+        <DownloadIcon
+          fontSize="large"
+          style={{ cursor: 'pointer', color: themeFont }}
+          onMouseDown={() => {
+            // Google Calendar Import
+            console.log('a');
+          }}
+        />
+      </div>
       <DashboardCalendar />
     </Page>
   );
