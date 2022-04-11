1dcd2b91f388d378fc2a13ac1f0007573ebe9731
Furkan Toprak <furkancemaltoprak@gmail.com>
2022-04-11 14:33:54 -0500

    fetchedStats

--- src/pages/Group.tsx
+++ src/pages/Group.tsx
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import Histogram from '../components/graphs/Histogram';
 import { RaspberryButton } from '../components/utils/Buttons';
 import Page from '../components/utils/Page';
 import Panel from '../components/utils/Panel';
 import { Body, Title } from '../components/utils/Texts';
 import FlaskClient from '../connections/Flask';
 import { useAuth } from '../contexts/Auth';
+import { purple, raspberry } from '../styles/Theme';
 import { Group } from './Groups';
 
+interface StatsSchema {
+  completed_hours_list: number[];
+  estimated_hours_list: number[];
+  num_completed_tasks: number[];
+  num_incompleted_tasks: number[];
+  total_week_hours: number[];
+}
 export default function GroupPage() {
   const navigate = useNavigate();
   const groupParams = useParams();
@@ -15,6 +24,7 @@ export default function GroupPage() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const groupId = groupParams.groupid || '';
   const [thisGroup, setThisGroup] = useState(null as null | false | Group);
+  const [groupStats, setGroupStats] = useState(null as null | StatsSchema);
   const leaveGroup = async () => {
     if (user === null || thisGroup === null || thisGroup === false) {
       return;
@@ -28,12 +38,21 @@ export default function GroupPage() {
     }
     navigate('/groups');
   };
-  useEffect(() => {
+  const fetchGroup = async () => {
     if (thisGroup !== null) {
+      console.log('a');
       return;
     }
+    const fetchedStats: StatsSchema = await FlaskClient.post('groups/getStats', {
+      group_id: groupId,
+    });
+
     // TODO: fetch group with group ID
+    setGroupStats(fetchedStats);
     setThisGroup(false);
+  };
+  useEffect(() => {
+    fetchGroup();
   }, [thisGroup]);
   if (thisGroup === null || thisGroup === false) {
     return <div />;
@@ -43,6 +62,14 @@ export default function GroupPage() {
       <Panel fill flex="column" centerY>
         <Title>MATH 411</Title>
         <Body>A group for MATH 411.</Body>
+        {
+          groupStats && (
+          <>
+            <Histogram title="Completed Time" data={groupStats.completed_hours_list} color={raspberry} />
+            <Histogram title="Estimated Time" data={groupStats.estimated_hours_list} color={purple} />
+          </>
+          )
+        }
         <RaspberryButton onMouseDown={() => {
           leaveGroup();
         }}
