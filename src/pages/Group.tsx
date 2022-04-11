07a0c0e9ac5e53aae83d0a9462f11f2253c90f74
Furkan Toprak <furkancemaltoprak@gmail.com>
2022-04-10 23:11:25 -0500

    leave group done.

--- src/pages/Group.tsx
+++ src/pages/Group.tsx
@@ -1,20 +1,38 @@
-import { group } from 'console';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { RaspberryButton } from '../components/utils/Buttons';
 import Page from '../components/utils/Page';
 import Panel from '../components/utils/Panel';
 import { Body, Title } from '../components/utils/Texts';
+import FlaskClient from '../connections/Flask';
+import { useAuth } from '../contexts/Auth';
 import { Group } from './Groups';
 
-export default function Group() {
+export default function GroupPage() {
+  const navigate = useNavigate();
   const groupParams = useParams();
+  const { user } = useAuth();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const groupId = groupParams.groupid || '';
   const [thisGroup, setThisGroup] = useState(null as null | false | Group);
+  const leaveGroup = async () => {
+    if (user === null || thisGroup === null || thisGroup === false) {
+      return;
+    }
+    const leaveResponse = await FlaskClient.post('groups/leaveGroup', {
+      user_id: user.id,
+      group_id: thisGroup.id,
+    });
+    if (leaveResponse.success === false) {
+      // TODO:
+    }
+    navigate('/groups');
+  };
   useEffect(() => {
     if (thisGroup !== null) {
       return;
     }
+    // TODO: fetch group with group ID
     setThisGroup(false);
   }, [thisGroup]);
   if (thisGroup === null || thisGroup === false) {
@@ -25,7 +43,13 @@ export default function Group() {
       <Panel fill flex="column" centerY>
         <Title>MATH 411</Title>
         <Body>A group for MATH 411.</Body>
-        <RaspberryButton>Leave Group</RaspberryButton>
+        <RaspberryButton onMouseDown={() => {
+          leaveGroup();
+        }}
+        >
+          Leave Group
+
+        </RaspberryButton>
       </Panel>
     </Page>
   );
